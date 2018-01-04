cc3007bf55d84d7156f6683f33d9514cb998a843
jt wang <jtjtjtjtw@gmail.com>
2018-01-04 13:21:56 -0600

    i can draw on photo?

--- client/components/editphoto.js
+++ client/components/editphoto.js
@@ -8,7 +8,9 @@ import history from '../history'
 import axios from 'axios'
 import Webcam from 'react-webcam'
 // import { ImageAnnotatorClient } from '@google-cloud/vision/src/v1';
-import {SketchField, Tools} from 'react-sketch';
+// import {SketchField, Tools} from 'react-sketch';
+import { SketchPad, TOOL_PENCIL, TOOL_LINE, TOOL_RECTANGLE, TOOL_ELLIPSE } from './../src';
+
 
 /**
  * COMPONENT
@@ -18,7 +20,13 @@ class Edit extends Component {
     super(props)
     this.state = {
       file: '',
-      imagePreviewUrl: ''
+      imagePreviewUrl: '',
+      tool: TOOL_PENCIL,
+      size: 2,
+      color: '#000000',
+      fill: false,
+      fillColor: '#444444',
+      items: []
     }
     this.setState = this.setState.bind(this)
   }
@@ -46,20 +54,79 @@ class Edit extends Component {
     imageObj1.onload = function(){
       ctx.drawImage(imageObj1, 0, 0)
     }
-
   }
 
   render(){
+    const { file, imagePreviewUrl, tool, size, color, fill, fillColor, items } = this.state;
+
     return (
       <div>
         <h2>
         edit photo
         </h2>
-          <canvas
+          {/*<canvas
           id="myCanvas"
           width={this.state.width}
           height={this.state.height}
+          />*/}
+
+          <div style={{float:'left', marginRight:20}}>
+          <SketchPad
+            width={500}
+            height={500}
+            animate={true}
+            size={size}
+            color={color}
+            fillColor={fill ? fillColor : ''}
+            items={items}
+            tool={tool}
+            onCompleteItem={(i) => wsClient.emit('addItem', i)}
           />
+        </div>
+        <div style={{float:'left'}}>
+          <div className="tools" style={{marginBottom:20}}>
+            <button
+              style={tool == TOOL_PENCIL ? {fontWeight:'bold'} : undefined}
+              className={tool == TOOL_PENCIL  ? 'item-active' : 'item'}
+              onClick={() => this.setState({tool:TOOL_PENCIL})}
+            >Pencil</button>
+            <button
+              style={tool == TOOL_LINE ? {fontWeight:'bold'} : undefined}
+              className={tool == TOOL_LINE  ? 'item-active' : 'item'}
+              onClick={() => this.setState({tool:TOOL_LINE})}
+            >Line</button>
+            <button
+              style={tool == TOOL_ELLIPSE ? {fontWeight:'bold'} : undefined}
+              className={tool == TOOL_ELLIPSE  ? 'item-active' : 'item'}
+              onClick={() => this.setState({tool:TOOL_ELLIPSE})}
+            >Ellipse</button>
+            <button
+              style={tool == TOOL_RECTANGLE ? {fontWeight:'bold'} : undefined}
+              className={tool == TOOL_RECTANGLE  ? 'item-active' : 'item'}
+              onClick={() => this.setState({tool:TOOL_RECTANGLE})}
+            >Rectangle</button>
+          </div>
+          <div className="options" style={{marginBottom:20}}>
+            <label htmlFor="">size: </label>
+            <input min="1" max="20" type="range" value={size} onChange={(e) => this.setState({size: parseInt(e.target.value)})} />
+          </div>
+          <div className="options" style={{marginBottom:20}}>
+            <label htmlFor="">color: </label>
+            <input type="color" value={color} onChange={(e) => this.setState({color: e.target.value})} />
+          </div>
+          {(this.state.tool == TOOL_ELLIPSE || this.state.tool == TOOL_RECTANGLE) ?
+            <div>
+              <label htmlFor="">fill in:</label>
+              <input type="checkbox" value={fill} style={{margin:'0 8'}}
+                     onChange={(e) => this.setState({fill: e.target.checked})} />
+              {fill ? <span>
+                  <label htmlFor="">with color:</label>
+                  <input type="color" value={fillColor} onChange={(e) => this.setState({fillColor: e.target.value})} />
+                </span> : ''}
+            </div> : ''}
+        </div>
+
+
       </div>
     )
   }
---
+++ client/src/index.js
@@ -0,0 +1,17 @@
+
+export {
+  default as SketchPad,
+  toolsMap
+} from './SketchPad.jsx';
+
+// TOOLS
+export {
+  Pencil,
+  TOOL_PENCIL,
+  Line,
+  TOOL_LINE,
+  Ellipse,
+  TOOL_ELLIPSE,
+  Rectangle,
+  TOOL_RECTANGLE
+} from './tools';
